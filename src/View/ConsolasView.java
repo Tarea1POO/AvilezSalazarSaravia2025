6477f9e67d44e55210f902c97215c817e0fffb58
Camila760467 <camila.salazar2401@alumnos.ubiobio.cl>
2025-06-15 18:43:12 -0400

    Update ConsolasView.java

--- src/View/ConsolasView.java
+++ src/View/ConsolasView.java
@@ -1,55 +1,145 @@
 package View;
-
 import Model.Consolas;
-import Model.Controles;
+import Model.Jugadores;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ConsolasView {
-    private Scanner tcld = new Scanner(System.in);
+
+    private Scanner teclado=new Scanner(System.in);
 
     public void mostrarMenu(){
-        System.out.println("Menu Consolas");
-        System.out.println("1- Crear Consolas");
-        System.out.println("2- Mostrar Consolas");
-        System.out.println("3- Actualizar Consolas");
-        System.out.println("4- Eliminar Consolas");
-        System.out.println("5- Salir");
-        System.out.println("Indicar una opción ❤:  ");
+        System.out.println(" ");
+        System.out.println(" MENÚ DE CONSOLAS ");
+        System.out.println("1.- Listar Consolas 🗒️ ");
+        System.out.println("2.- Crear Consola 🔧");
+        System.out.println("3.- Actualizar Consola 🛠️");
+        System.out.println("4.- Eliminar Consola ❌ ");
+        System.out.println("5.- Salir 🔑 ");
+        System.out.print("Indique una opción 😊: ");
     }
-    public void mostrarConsolas(List<Consolas> consolas){
-        if(consolas.isEmpty()){
-            System.out.println("No hay jugadores");
-        }else{
+
+    
+    public void mostrarConsolas (List<Consolas> consolas){
+        if( consolas.isEmpty()){ //SI LA LISTA ESTA VACÍA
+            System.out.println("No hay consolas");
+
+        } else{ 
+           
             consolas.forEach(System.out::println);
         }
     }
-    public Consolas leerNuevoConsolas(){
-        System.out.println("nombre: ");
-        String nombre = tcld.nextLine();
-        System.out.println("fabricante: ");
-        String fabricante = tcld.nextLine();
-        System.out.println("anio_lanzamiento: ");
-        int anio = Integer.parseInt(tcld.nextLine());
-        Consolas consolas = new Consolas ();
+
+    
+    public Consolas leerNuevaConsola() {
+
+        
+        String nombre="";
+        while(nombre.trim().isEmpty()){
+            System.out.print("Nombre: ");
+            nombre=teclado.nextLine();
+            if(nombre.trim().isEmpty()){
+                System.out.println("⚠️ Campo requerido");
+            }
+        }
+
+       
+        String fabricante="";
+        while(fabricante.trim().isEmpty()) {
+            System.out.print("Fabricante: ");
+            fabricante = teclado.nextLine();
+            if (fabricante.trim().isEmpty()) {
+                System.out.println("⚠️ Campo requerido");
+            }
+        }
+
+       
+        int anio_lanzamiento=-1;
+        while(true){
+            System.out.print("Año de lanzamiento: ");
+            String entradaUser= teclado.nextLine().trim();
+
+            //VALIDAR QUE NO SEA UN CAMPO VACÍO
+            if(entradaUser.isEmpty()){
+                System.out.println("⚠️ Campo requerido");
+                continue; 
+            }
+            try{
+                anio_lanzamiento=Integer.parseInt(entradaUser);
+                if(anio_lanzamiento <=0){
+                    System.out.println("⚠️ Solo debe ingresar números enteros positivos");
+                    continue; 
+
+                }
+                break; 
+            }catch(NumberFormatException e){
+                System.out.println("⚠️ Solo debe ingresar dígitos");
+            }
+        }
+
+       
+        Consolas consolas= new Consolas();
         consolas.setNombre(nombre);
         consolas.setFabricante(fabricante);
-        consolas.setAnio_lanzamiento(anio);
+        consolas.setAnio_lanzamiento(anio_lanzamiento);
         return consolas;
     }
-    public Consolas leerConsolasACtualizar(){
-        System.out.println("Ingrese el ID del controles a actualizar");
-        int id = Integer.parseInt(tcld.nextLine());
-        Consolas consolas = leerConsolasACtualizar();
+
+
+   
+    public Consolas leerConsolaActualizadas(int id){
+        Consolas consolas=leerNuevaConsola();
+        //SE ASIGNA EL ID DE LA CONSOLA QUE SE ESTA MODIFICANDO
         consolas.setId_consola(id);
         return consolas;
     }
-    public int leerIdEliminar(){
-        System.out.println("Ingrese el id a eliminar: ");
-        return Integer.parseInt(tcld.nextLine());
+
+   
+    public int leerIdEliminar() {
+        int id = -1;
+        while (true) {
+            System.out.print("Ingrese el ID a eliminar: ");
+            String entradaUser = teclado.nextLine().trim();
+            if(entradaUser.isEmpty()){
+                System.out.println("⚠️ Campo requerido");
+                continue;
+            }
+            try {
+                id = Integer.parseInt(entradaUser);
+                if (id <= 0) {
+                    System.out.println("⚠️ Solo debe ingresar números enteros positivos");
+                    continue;
+                }
+                break;
+            }catch(NumberFormatException e){
+                System.out.println("⚠️ Solo debe ingresar dígitos");
+            }
+        }
+        return id;
     }
+
+   
+    public int leerIdActualizar(){
+        System.out.print("Ingrese el ID de la consola a actualizar:");
+        return Integer.parseInt(teclado.nextLine());
+    }
+
+   
     public int leerOpcion(){
-        return Integer.parseInt(tcld.nextLine());
+        int opcion=-1;
+        while(true){
+            String entradaUser=teclado.nextLine().trim();
+            if(entradaUser.isEmpty()){
+                System.out.println("⚠️ Campo requerido");
+                continue;
+            }
+            try{
+                opcion=Integer.parseInt(entradaUser);
+                break;
+            }catch(NumberFormatException e){
+                System.out.println("⚠️ Ingresar un numero del [1-5]");
+            }
+        }
+        return opcion;
     }
 }
